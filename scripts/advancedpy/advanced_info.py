250fae1bde7f03eef7972f8a06f74ad062120f92
draziotis <drazioti@gmail.com>
2018-04-07 18:30:02 +0300

    ,

--- scripts/advancedpy/advanced_info.py
+++ scripts/advancedpy/advanced_info.py
@@ -2,31 +2,24 @@
 """
 Created on Sat Mar 31 02:58:23 2018
 Written in python 3.4.2
-Script for finding infos of your box
-1.Try to find basic programs installed in the system
--gcc, perl, python, nmap, netcat, wget, ftp
-2. Find fles that have the sticky bit
-3. Find files with the name passwords
-4. 
 
+===TODO===
+
+1. Try to find basic programs installed in the system
+-gcc, perl, python, nmap, netcat, wget, ftp, ssh, scp
+2. Find fles that have the sticky bit
+3. Find files which contain the name "password"
+4. Find if some servers are running
 
 
 Libraries used
 --------------
-os,subprocess,grp,pwd
 
 Calls to external programs
 --------------------------
-awk
 
-Run with the foloowing order
-find_distro()
-find_version()
-find_cpu('True')
-find_mem('True')  
-disks()
-find_desktop_enviroment()
-users()
+
+
 
 
 """
