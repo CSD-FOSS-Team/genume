8b197b947ef86478fdb8426020ecf3073a8ae687
MaanooAk <akritasak@gmail.com>
2018-04-12 01:04:07 +0300

    Introduced list entries

--- genume/registry/xparser.py
+++ genume/registry/xparser.py
@@ -4,7 +4,8 @@ import os
 
 from genume.constants import VERSION
 from genume.registry.base import InfoLevel
-from genume.registry.value import ValueEntry
+from genume.registry.value import ValueEntry, ListEntry
+
 
 def parse(cmdl, c):
     while len(cmdl) != 0:
@@ -17,8 +18,23 @@ def parse(cmdl, c):
             if e == "ADV":
                 v.level = InfoLevel.advanced
             c[k] = v
+        elif cmd == "VALUES":
+            # entry of list
+            e = cmdl.pop(0)
+            k = cmdl.pop(0)
+            v = cmdl.pop(0).strip()
+
+            if k in c:
+                c[k].add(v)
+            else:
+                e = ListEntry(c, v)
+                if e == "ADV":
+                    e.level = InfoLevel.advanced
+                c[k] = e
+
         else:
             print("Warn: invalid command {0}".format(cmd))
+
 
 def run(f):
     if os.access(str(f), os.X_OK):
@@ -28,10 +44,12 @@ def run(f):
     else:
         print("Warn: {0} is not executable.".format(str(f)))
 
+
 def run_and_parse(cat, file):
     s = run(file)
     if s:
         parse(cmdsplit(s), cat)
 
 # Initialize environment variables.
+# TODO move elsewhere
 os.putenv("GENUME_VERSION", VERSION)
--- scripts/PROTOCOL.md
+++ scripts/PROTOCOL.md
@@ -43,6 +43,13 @@ Entries are created by parsing the output of the script. As a result it must out
     - `key` is the key. It should contain only characters you would use to name a variable _(aka alphanumeric and underscores)_.
     - `value` is the string to display. It must be between double quotation marks.
 
+1. `VALUES [BAS|ADV] key value`
+    - Creates a new entry containing a list of strings.
+    - `VALUES` is the command name.
+    - `[BAS|ADV]` is an enum. It is either `BAS` for **basic** or information or `ADV` for **advanced** information.
+    - `key` is the key. It should contain only characters you would use to name a variable _(aka alphanumeric and underscores)_.
+    - `value` is one of the strings to display. It must be between double quotation marks.
+
 ### Example script
 
 ```sh
