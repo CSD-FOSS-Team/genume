e15a70af5a2b784b6d2fdec9e31edf4c1a95e798
MaanooAk <akritasak@gmail.com>
2018-04-20 11:38:30 +0300

    Dynamic subtrees (list view) generations

--- genume/view/main.py
+++ genume/view/main.py
@@ -5,6 +5,7 @@ from gi.repository import Gtk, Gdk
 
 from genume.registry.registry import Registry
 from genume.registry.category import CategoryEntry
+from genume.terminterface import print_enumeration
 
 
 def main():
@@ -23,33 +24,49 @@ class MainWindow(Gtk.Window):
         # setup the layout
 
         roots_container = self.generate_roots_container()
-        grid.add(roots_container)
+        grid.pack_start(roots_container, False, False, 0)
         subtrees_container = self.generate_subtrees_container()
-        grid.add(subtrees_container)
+        grid.pack_start(subtrees_container, True, True, 0)
 
         # fill the layout
 
+        # TODO improve, covert Registry to a signleton
         reg = Registry()
         reg.update()
+        # TODO remove, here for debugging
+        print_enumeration(reg.root)
 
         for name, entry in reg.root.items():
             if isinstance(entry, CategoryEntry):
 
-                root = self.generate_root(name, entry)
-                roots_container.add(root)
-
-                # subtree = self.generate_subtree(name, entry)
-                # subtrees_container.add(subtree)
+                self.generate_root_and_subtree(name, entry,
+                        roots_container, subtrees_container)
             else:
-                print("Scripts on the root scripts folder are not supported, yet") # TODO implement
+                print("Scripts on the root scripts folder are not supported, yet")  # TODO implement
 
         # handle events
-
         self.connect("destroy", Gtk.main_quit)
         self.show_all()
 
+        # store state
+        self.subtrees_container = subtrees_container
+
+
+    def generate_root_and_subtree(self, name, entry: CategoryEntry, roots_container, subtrees_container):
+
+        root = self.generate_root(name, entry)
+        roots_container.add(root)
+
+        subtree = self.generate_subtree(name, entry)
+        subtrees_container.append_page(subtree, Gtk.Label(label=name))
+
+        # setup the events
+        root.page_index = subtrees_container.get_n_pages() - 1
+        root.connect("clicked", self.show_root)
+
+
     def generate_roots_container(self):
-        
+
         return Gtk.Box(
             orientation=Gtk.Orientation.VERTICAL,
             spacing=6,
@@ -65,12 +82,32 @@ class MainWindow(Gtk.Window):
 
     def generate_subtrees_container(self):
 
-        pass
+        return Gtk.Notebook(
+            show_tabs=False
+        )
 
     def generate_subtree(self, name, entry: CategoryEntry):
         """Generate the list like view that correspond to the given entry"""
 
-        pass
+        # create the store
+
+        store = Gtk.ListStore(str, str)
+        for name, entry in entry.items():
+            if isinstance(entry, CategoryEntry):
+                print("Scripts on the sub root folders are not supported, yet")  # TODO implement
+            else:
+                store.append([name, repr(entry)])
+
+        # create the tree view
+
+        tree = Gtk.TreeView(store)
+        for i, column_title in enumerate(["Name", "Value"]):
+            tree.append_column(Gtk.TreeViewColumn(
+                column_title,
+                Gtk.CellRendererText(),
+                text=i
+            ))
+        return tree
 
     def create_textview(self):
         scrolledwindow = Gtk.ScrolledWindow()
@@ -82,3 +119,7 @@ class MainWindow(Gtk.Window):
         self.textbuffer = self.textview.get_buffer()
         self.textview.set_editable(False)
         self.textview.set_cursor_visible(False)
+
+    def show_root(self, button):
+        self.subtrees_container.set_current_page(button.page_index)
+
