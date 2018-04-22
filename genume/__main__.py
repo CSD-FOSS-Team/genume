f9d8f6b583f225abb949ca1386791fe6da3b2493
Sima214 <anastasios2014@gmail.com>
2018-04-23 00:00:28 +0300

    Closes CSD-FOSS-Team/genume#23 Also this will probably result in a merge conflict.

--- genume/__main__.py
+++ genume/__main__.py
@@ -4,25 +4,18 @@
 # $ python genume/__main__.py (2.6+)
 # $ python -m genume          (2.7+)
 
-# TODO import Gtk only if the gui will be shown
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-
 import sys
 import os.path
+import logging
+
+from genume.logging.logger import init as initLogger
+from genume.genume import main
 
 if __package__ is None and not hasattr(sys, 'frozen'):
     # direct call of __main__.py
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
-import genume.terminterface as cli
-import genume.view.main as gui
-
 if __name__ == '__main__':
-
-    # cli.main()
-
-    gui.main()
-    Gtk.main()
+    initLogger()
+    main()
