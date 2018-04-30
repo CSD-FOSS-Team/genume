5edbca15ac37d98ac72e9180b48258a1211d4cf2
damavrom <damavrom@gmail.com>
2018-04-30 14:28:18 +0300

    removed echo

--- scripts/power/dc.sh
+++ scripts/power/dc.sh
@@ -7,7 +7,6 @@ dir=$(find /sys/ -name power_supply -type d 2>/dev/null)
 #distinguises the dc input and it assumes there is only one
 dir=$(grep -lire Mains $dir 2>/dev/null | head -n 1)
 cnt=0
-echo $dir
 curdir=${dir%/*}
 
 if [ -f ${curdir}/online ]
