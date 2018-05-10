92c4311e73831a65ba303cbc37d991e6699da12d
Sima214 <anastasios2014@gmail.com>
2018-05-10 22:06:07 +0300

    Rebasing

--- genume/registry/category.py
+++ genume/registry/category.py
@@ -1,7 +1,36 @@
+<<<<<<< HEAD
+=======
+from collections.abc import MutableMapping
+>>>>>>> 88ed0ef... Rebasing
 from collections import OrderedDict
 
-from genume.registry.base import BaseEntry
+from genume.registry.base import BaseEntry, InfoLevel
 
 
+<<<<<<< HEAD
 class CategoryEntry(BaseEntry, OrderedDict):
+=======
+class CategoryEntry(BaseEntry):
+>>>>>>> 88ed0ef... Rebasing
     """A registry entry which acts as a container for other entries."""
+    def __init__(self, parent=None, level=InfoLevel.basic):
+        self.storage = OrderedDict()
+        super().__init__(parent, level)
+        
+    def __getitem__(self, key):
+        return self.storage[self.__keytransform__(key)]
+
+    def __setitem__(self, key, value):
+        self.storage[self.__keytransform__(key)] = value
+
+    def __delitem__(self, key):
+        del self.storage[self.__keytransform__(key)]
+
+    def __iter__(self):
+        return iter(self.storage)
+
+    def __len__(self):
+        return len(self.storage)
+
+    def __keytransform__(self, key):
+        return key
