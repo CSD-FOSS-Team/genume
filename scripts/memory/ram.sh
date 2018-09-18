cb21cbbfbcfa29f7f493f0b8b035ad1c62e938d4
MaanooAk <akritasak@gmail.com>
2018-09-18 14:37:10 +0300

    Fixed ram script

--- scripts/memory/ram.sh
+++ scripts/memory/ram.sh
@@ -6,5 +6,5 @@ freeMem=$(grep Mem /proc/meminfo | head -n 2 | tail -n 1 | cut -f 2 -d : )
 swapMem=$(grep Mem /proc/meminfo | head -n 3 | tail -n 1 | cut -f 2 -d : )
 
 echo VALUE BAS memory_total \" $totalMem \"
-echo VALUE BAS memory_free \" freeMem \"
+echo VALUE BAS memory_free \" $freeMem \"
 echo VALUE BAS memory_swap \" $swapMem \"
