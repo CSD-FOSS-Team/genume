39825777491274c85db10a90f0386191de49cc18
leonidas <leonidaa@csd.auth.gr>
2018-09-29 19:55:11 +0300

    Updated memory scripts

--- scripts/memory/ram.sh
+++ scripts/memory/ram.sh
@@ -5,7 +5,6 @@ totalMem=$(grep Mem /proc/meminfo | head -n 1 | cut -f 2 -d : )
 freeMem=$(grep Mem /proc/meminfo | head -n 2 | tail -n 1 | cut -f 2 -d : )
 swapMem=$(grep Mem /proc/meminfo | head -n 3 | tail -n 1 | cut -f 2 -d : )
 
-echo GROUP BAS ram
 echo VALUE BAS memory_total \" $totalMem \"
-echo VALUE BAS memory_free \" $freeMem \"
+echo VALUE BAS memory_free \" freeMem \"
 echo VALUE BAS memory_swap \" $swapMem \"
