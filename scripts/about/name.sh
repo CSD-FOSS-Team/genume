be82edd8ff0f441f86eb1554ff2b78c51e0f0d51
draziotis <drazioti@gmail.com>
2018-12-04 05:39:54 +0200

    change the section genume

--- scripts/about/name.sh
+++ scripts/about/name.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# testing list values
+
+echo VALUE BAS genume 'Graphical\ ENUMeration\ ver.\ 1.0' 
+echo VALUE BAS Team 'CSD\ foss\ team,\ https://foss.csd.auth.gr' 
+# testing list values 
 git log --pretty="VALUES BAS authors \"%an %ce\"" | sort | uniq | grep -v noreply
 
 # testing changing values
