a1761255cdff8b4eb2c577f54e33dfee46092de9
draziotis <drazioti@gmail.com>
2018-12-05 00:58:21 +0200

    .

--- scripts/about/name.sh
+++ scripts/about/name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo VALUE BAS genume 'Graphical\ ENUMeration\ ver.\ 1.0' 
+echo VALUE BAS GENUME 'Graphical\ ENUMEration\ ver.\ 1.0' 
 echo VALUE BAS Team 'CSD\ foss\ team,\ https://foss.csd.auth.gr' 
 # testing list values 
 git log --pretty="VALUES BAS authors \"%an %ce\"" | sort | uniq | grep -v noreply
