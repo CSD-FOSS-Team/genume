60afa55132f9fc138bf7d71cbb61e3dedf32da05
MaanooAk <akritasak@gmail.com>
2018-12-08 12:55:37 +0200

    Cache the opengl info command output

--- scripts/gpu/opengl.sh
+++ scripts/gpu/opengl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-OpenGlVendor=$(glxinfo| grep "OpenGL vendor string"| head -n 1|cut -d ":" -f 2)
-OpenGLRendererString=$(glxinfo| grep "OpenGL renderer string"| head -n 1|cut -d ":" -f 2)
-OpenGLVersion=$(glxinfo|grep "OpenGL version"| head -n 1|cut -d ":" -f 2)
-OpenGLShadLangVersion=$(glxinfo|grep "OpenGL version string"| head -n 1|cut -d ":" -f 2)
-OpenGLContextFlags=$(glxinfo|grep "OpenGL context"| head -n 1|cut -d ":" -f 2)
-OpenGLExtension=$(glxinfo|grep "OpenGL extensions"| head -n 1|cut -d ":" -f 2)
+info="$(glxinfo)"
+
+OpenGlVendor=$(echo "$info"| grep "OpenGL vendor string"| head -n 1|cut -d ":" -f 2)
+OpenGLRendererString=$(echo "$info"| grep "OpenGL renderer string"| head -n 1|cut -d ":" -f 2)
+OpenGLVersion=$(echo "$info"|grep "OpenGL version"| head -n 1|cut -d ":" -f 2)
+OpenGLShadLangVersion=$(echo "$info"|grep "OpenGL version string"| head -n 1|cut -d ":" -f 2)
+OpenGLContextFlags=$(echo "$info"|grep "OpenGL context"| head -n 1|cut -d ":" -f 2)
+OpenGLExtension=$(echo "$info"|grep "OpenGL extensions"| head -n 1|cut -d ":" -f 2)
 
 
 echo GROUP BAS opengl
@@ -17,10 +19,10 @@ echo VALUE ADV OpenGL_Context_Flags \"$OpenGLContextFlags\"
 echo VALUE ADV OpenGL_Extension \"$OpenGLExtension\"
 
 
-OpenGLCoreProfVersion=$(glxinfo|grep "OpenGL core profile version"| head -n 1|cut -d ":" -f 2)
-OpenGLCoreProfShadLang=$(glxinfo|grep "OpenGL core profile shading"|head -n 1|cut -d ":" -f 2)
-OpenGLCoreProfMask=$(glxinfo|grep "OpenGL core profile profile mask"|head -n 1|cut -d ":" -f 2)
-OpenGLCoreProfContext=$(glxinfo|grep "OpenGL core profile context"|head -n 1|cut -d ":" -f 2)
+OpenGLCoreProfVersion=$(echo "$info"|grep "OpenGL core profile version"| head -n 1|cut -d ":" -f 2)
+OpenGLCoreProfShadLang=$(echo "$info"|grep "OpenGL core profile shading"|head -n 1|cut -d ":" -f 2)
+OpenGLCoreProfMask=$(echo "$info"|grep "OpenGL core profile profile mask"|head -n 1|cut -d ":" -f 2)
+OpenGLCoreProfContext=$(echo "$info"|grep "OpenGL core profile context"|head -n 1|cut -d ":" -f 2)
 
 echo GROUP BAS opengl
 echo VALUE ADV OpenGl_Core_Profile_Version \"$OpenGLCoreProfVersion\"
@@ -28,9 +30,9 @@ echo VALUE ADV OpenGl_Core_Profile_Shanding_Language_Version \"$OpenGLCoreProfSh
 echo VALUE ADV OpenGl_Core_Profile_Mask \"$OpenGLCoreProfMask\"
 echo VALUE ADV OpenGl_Core_Profile_Context_Flags \"$OpenGLCoreProfContext\"
 
-OpenGLESProfile=$(glxinfo|grep "OpenGL ES profile version"| head -n 1|cut -d ":" -f 2)
-OpenGLESProfileExten=$(glxinfo|grep "OpenGL ES profile extensions"| head -n 1|cut -d ":" -f 2)
-OpenGLESProfileShadLang=$(glxinfo|grep "OpenGL ES profile shading"| head -n 1|cut -d ":" -f 2)
+OpenGLESProfile=$(echo "$info"|grep "OpenGL ES profile version"| head -n 1|cut -d ":" -f 2)
+OpenGLESProfileExten=$(echo "$info"|grep "OpenGL ES profile extensions"| head -n 1|cut -d ":" -f 2)
+OpenGLESProfileShadLang=$(echo "$info"|grep "OpenGL ES profile shading"| head -n 1|cut -d ":" -f 2)
 
 echo PATH BAS opengl.es
 echo VALUE ADV OpenGl_ES_Profile_Version \"$OpenGLESProfile\"
