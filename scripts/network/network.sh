d55a1daeb3d6ae51783273c731ee3518f85abc55
Winner5001 <johnchris1998@windowslive.com>
2019-01-04 16:45:50 +0200

    Fixed Network script

--- scripts/network/network.sh
+++ scripts/network/network.sh
@@ -1,53 +1,62 @@
 #!/bin/bash
 
 wcnt=0
+echo $wcnt > /tmp/wirelessgenume
 ecnt=0
+echo $ecnt > /tmp/ethernetgenume
 echo PATH BAS Ethernet
 echo PATH BAS Wireless
+wireless=''
+ethernet=''
 lspci | egrep -i 'Network|Ethernet|802.11' | while read i
 do
 	a=${i#*: }
 	if [ $(echo $a | egrep -i "Wireless|802.11" | wc -l) -eq 1 ]; then
 		wireless=$(echo $a)
-		name=${wireless}
-		wcnt=$[ $wcnt + 1]
+		wcnt=$(cat /tmp/wirelessgenume)
+		wcnt=$(($wcnt + 1))
+		echo $wcnt > /tmp/wirelessgenume
+		
 	elif [ $(echo $a | grep -i "Ethernet" | wc -l) -eq 1 ]; then
 		ethernet=$(echo $a)
-		name=${ethernet}
-		ecnt=$[ $ecnt + 1]
+		ecnt=$(cat /tmp/ethernetgenume)
+		ecnt=$(($ecnt + 1))
+		echo $ecnt > /tmp/ethernetgenume
 	fi
-	
 	if [  ! -z "${wireless}" ]
 	then
-		echo PATH BAS Wireless.Wireless_$[$wcnt]
+		echo PATH BAS Wireless.Wireless_$wcnt
 		echo VALUE BAS Wireless \"${wireless}\"
 	elif [  ! -z "${ethernet}" ]
 	then
-		echo PATH BAS Ethernet.Ethernet_$[$ecnt]
+		echo PATH BAS Ethernet.Ethernet_$ecnt
 		echo VALUE BAS Ethernet \"${ethernet}\"
 	fi
 done
-
 lsusb | egrep -i "Network|Wireless|Ethernet|802.11" | while read i
 do
 	a=${i#*: }
 	if [ $(echo $a | egrep -i "Wireless|802.11" | wc -l) -eq 1 ]; then
 		wireless=$(echo $a)
-		name=${wireless}
-		wcnt=$[ $wcnt + 1]
+		wcnt=$(cat /tmp/wirelessgenume)
+		wcnt=$(($wcnt + 1))
+		echo $wcnt > /tmp/wirelessgenume
 	elif [ $(echo $a | grep -i "Ethernet" | wc -l) -eq 1 ]; then
 		ethernet=$(echo $a)
-		name=${ethernet}
-		ecnt=$[ $ecnt + 1]
+		ecnt=$(cat /tmp/ethernetgenume)
+		ecnt=$(($ecnt + 1))
+		echo $ecnt > /tmp/ethernetgenume
 	fi
 
 	if [  ! -z "${wireless}" ]
 	then
-		echo PATH BAS Wireless.Wireless_$[$wcnt]
+		echo PATH BAS Wireless.Wireless_$wcnt
 		echo VALUE BAS Wireless \"${wireless}\"
 	elif [  ! -z "${ethernet}" ]
 	then
-		echo PATH BAS Ethernet.Ethernet_$[$ecnt]
+		echo PATH BAS Ethernet.Ethernet_$ecnt
 		echo VALUE BAS Ethernet \"${ethernet}\"
 	fi
 done
+rm /tmp/wirelessgenume
+rm /tmp/ethernetgenume
