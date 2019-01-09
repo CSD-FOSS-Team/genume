0e452577bb148de40416e4ee4a096345650e3507
xlxs4 <oorestis@csd.auth.gr>
2019-01-09 16:02:32 +0200

    better format text layout and add widely used linux apps

--- scripts/software/apps.sh
+++ scripts/software/apps.sh
@@ -23,18 +23,76 @@ check "git" "--version"
 
 check "gnome-software" "--version"
 
+'''
+Programming Languages & Compilers
+'''
+
+# GNU Compiler Collection (C, C++, Go...)
 check "gcc" "--version"
 check "java" "-version"
 check "python" "-V"
+# Haskell Compiler
+check "ghci" "--version"
 check "ruby" "-v"
 check "perl" "-v"
+check "lua" "-v"
 check "mysql" "--version"
+
+'''
+PenTest
+'''
+
 check "freedom" "-version"
+# Security Scanner
 check "nmap" "-V"
+
+''' 
+Text Editors
+'''
+
 check "nano" "--version"
 check "vi" "--version"
 check "emacs" "--version"
+check "code" "--version"
+check "atom" "--version"
+
+'''
+File Editors
+'''
+
+# Office Suite
 check "libreoffice" "--version"
+# PDF Viewer
+check "okular" "--version"
+# Typesetting System
+check "tex" "--version"
+# 3D CAD Modeler
+check "freecad" "--version"
 
-# TODO extend
+'''
+Window Managers
+'''
 
+check "i3" "--version"
+check "parted" "--version"
+check "stacer" "--version"
+check "transmission-cli" "--version"
+check "vlc" "--version"
+check "audacious" "--version"
+check "gimp" "--version"
+check "kodi" "--version"
+
+'''
+Browsers
+'''
+
+check "chromium" "--version"
+check "firefox" "--version"
+
+'''
+Email Applications
+'''
+
+check "thunderbird" "--version"
+
+# TODO: extend
